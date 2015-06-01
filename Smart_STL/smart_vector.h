d1f336b01a01949742b6aa19b2d9421af8ebef8a
markjenny <lijianlin1990@sina.com>
2015-06-01 21:59:34 +0800

    增加新的构造函数

--- Smart_STL/smart_vector.h
+++ Smart_STL/smart_vector.h
@@ -1,8 +1,9 @@
 #ifndef _SMART_VECTOR_H_
 #define _SMART_VECTOR_H_
-#include "../smart_iterator_base.h"
-#include "../smart_alloc.h"
-#include "../smart_construct.h"
+#include <iostream>
+#include "smart_iterator_base.h"
+#include "smart_alloc.h"
+#include "smart_construct.h"
 #include <cstddef> //for ptrdiff_t  and size_t,because you need to "define" the ptrdiff_t
 /*****暂时没有包含reverse_iterator相关功能******
 author:Mark
@@ -50,10 +51,10 @@ namespace smart_stl
 	public:
 		/*****涉及到构造函数、复制构造函数、析构函数**
 		*************************************************/
-		vector() : start_(0), finish_(0), end_of_storage_(0){}
-		//防止隐式调用构造函数，让接口具有统一性，“使得接口更加容易正确使用”
+		vector() : start_(0), finish_(0), end_of_storage_(0){std::cout << "调用了没有参数的构造函数" << std::endl;}
+		//防止隐式调用构造函数，让接口具有统一性，“使得接口更加容易正确使用”，注：explicit是声明修饰符
 		explicit vector(const size_type n);
-		vector(size_type n, const value_type& value);
+		vector(size_type n, const value_type& val);
 		//用于通过现有的数组来构造vector对象
 		template<class InputIterator>
 		vector(InputIterator first, InputIterator last);
@@ -62,6 +63,10 @@ namespace smart_stl
 		//“如果某函数要调用多个其他的函数，而其他的函数时普通函数，那么该函数没有必要设置成inline类型的”
 		//inline函数适合那些简短的，重复性高的函数，所以~vector()个人认为不适合做inline函数
 		~vector();
+// 		{
+// 			destroy(start_, finish_);
+// 			deallocate();
+// 		}
 
 
 		/***与比较相关************
@@ -85,7 +90,7 @@ namespace smart_stl
 		difference_type capacity() const {return end_of_storage_ - start_;}
 		bool empty() const {return start_ == finish_;}
 		void resize(size_type new_size);
-		void resize(size_type new_size, const T& value);
+		void resize(size_type new_size, const T& val);
 
 		/*******与访问元素有关***********
 		******************************/
@@ -115,9 +120,9 @@ namespace smart_stl
 
 	private:
 		//fill_initialize用于vector的构造函数
-		fill_initialize();
+		void fill_initialize(size_type n, const value_type& val);
 		//用于配置空间并将数据进行填充
-		allocate_and_fill(const size_type& n, const value_type& val);
+		void allocate_and_fill(size_type n, const value_type& val);
 		//后续要使用到构造和插入的辅助函数insert_aux,vector_aux
 		/*****************
 		此处待填写
@@ -127,7 +132,71 @@ namespace smart_stl
 
 
 
+
+
 	};
+	/**********************************vector相关实现***************************************************/
+	//由于模板类的函数声明和实现的其中一种方式是将声明与实现都放在.h文件中，一下便是vector的具体实现
+
+
+	template<class T, class Alloc>
+	vector<T, Alloc>::vector(const size_type n)
+	{
+		fill_initialize(n, T());
+	}
+
+	template<class T, class Alloc>
+	vector<T, Alloc>::vector(size_type n, const value_type& val)
+	{
+		fill_initialize(n, val);
+	}
+
+	template<class T, class Alloc>
+	template<class InputIterator>
+	vector<T, Alloc>::vector(InputIterator first, InputIterator last)
+	{
+
+	}
+
+
+	template<class T, class Alloc>
+	vector<T,Alloc>::~vector()
+	{
+		std::cout << "调用析构函数" << std::endl;
+	}
+
+
+
+	/*需要说明一点，关于模板类函数在类外定义的时候需要带有特定的头部，如：
+	template<class T, class Alloc>
+	vector<T, Alloc>::
+	但是在函数的参数中，确实可以直接用value_type而不是T，是因为我们已经在类的声明中define了value_type是什么意思，所以这里可以直接用
+	*/
+	template<class T, class Alloc>
+	void vector<T, Alloc>::fill_initialize(size_type n, const value_type& val)
+	{
+		start_ = allocate_and_fill(n, val);
+		finish_ = start_ + n;
+		end_of_storage_ = finish_;
+	}
+
+	template<class T, class Alloc>
+	iterator vector<T, Alloc>::allocate_and_fill(size_type n, const value_type& val)
+	{
+		iterator result = data_allocator::allocate(n);
+		uninitialized_fill_n(result, n, val);
+		result = result - n;
+	}
+
+	template<class T, class Alloc>
+	void vector<T, Alloc>::deallocate()
+	{
+		//判断start_是否为null
+		if (start_)
+		{
+			data_allocator::deallocate(start_, end_of_storage_ - start_);//end_of_storage_和end_符合前开后闭的特性
+		}
+	}
 }
 
 #endif	_SMART_VECTOR_H_
