d3dd9b190e7c96756576d04f435542835f771531
markjenny <lijianlin1990@sina.com>
2015-06-02 17:07:06 +0800

    smart_vector新增功能
    新增加了多种构造函数和复制构造函数

--- Smart_STL/smart_vector.h
+++ Smart_STL/smart_vector.h
@@ -4,6 +4,7 @@
 #include "smart_iterator_base.h"
 #include "smart_alloc.h"
 #include "smart_construct.h"
+#include "smart_uninitialized.h"
 #include <cstddef> //for ptrdiff_t  and size_t,because you need to "define" the ptrdiff_t
 /*****暂时没有包含reverse_iterator相关功能******
 author:Mark
@@ -49,30 +50,81 @@ namespace smart_stl
 		iterator end_of_storage_;
 
 	public:
-		/*****涉及到构造函数、复制构造函数、析构函数**
-		*************************************************/
-		vector() : start_(0), finish_(0), end_of_storage_(0){std::cout << "调用了没有参数的构造函数" << std::endl;}
+		/*****涉及到构造函数、复制构造函数、析构函数*/
+		
+		/*****
+		功能：vector构造函数
+		参数：无
+		返回值：无
+		******/
+		vector() : start_(0), finish_(0), end_of_storage_(0){}
+
+		/*****
+		功能：vector构造函数
+		参数：需要构造的元素个数n
+		返回值：无
+		******/
 		//防止隐式调用构造函数，让接口具有统一性，“使得接口更加容易正确使用”，注：explicit是声明修饰符
 		explicit vector(const size_type n);
+
+		/*****
+		功能：vector构造函数
+		参数：需要构造的元素个数n，需要构造的元素值val
+		返回值：无
+		******/
 		vector(size_type n, const value_type& val);
+
+		/*****
+		功能：vector构造函数
+		参数：被复制的vector的迭代器first和last
+		返回值：无
+		******/
 		//用于通过现有的数组来构造vector对象
 		template<class InputIterator>
 		vector(InputIterator first, InputIterator last);
+
+		/*****
+		功能：vector复制构造函数
+		参数：被复制的vector对象v
+		返回值：无
+		******/
 		vector(const vector& v);
-		vector& operator=(const vector&);
+
+		/*****
+		功能：vector的assignment operator
+		参数：被进行赋值操作的vector对象
+		返回值：无
+		******/
+		vector& operator=(const vector& v);
+
+		/*****
+		功能：vector析构函数
+		参数：无
+		返回值：无
+		******/
 		//“如果某函数要调用多个其他的函数，而其他的函数时普通函数，那么该函数没有必要设置成inline类型的”
 		//inline函数适合那些简短的，重复性高的函数，所以~vector()个人认为不适合做inline函数
 		~vector();
-// 		{
-// 			destroy(start_, finish_);
-// 			deallocate();
-// 		}
+
+		/*************************************************/
 
 
-		/***与比较相关************
-		************************/
-		bool operator==(const vector& ) const;
-		bool operator!=(const vector&) const;
+		/****************************************************与比较相关*************************************************************/
+		/*****
+		功能：判断两个vector对象是否相同
+		参数：被用来比较的vector对象v
+		返回值：是否相同的bool型结果
+		******/
+		bool operator==(const vector& v) const;
+
+		/*****
+		功能：判断两个vector对象是否不同
+		参数：被用来比较的vector对象v
+		返回值：是否不同的bool型结果
+		******/
+		bool operator!=(const vector& v) const;
+
+		/**************************************************************************************************************************/
 
 		/*****与迭代器相关*********
 		*****************************/
@@ -122,7 +174,7 @@ namespace smart_stl
 		//fill_initialize用于vector的构造函数
 		void fill_initialize(size_type n, const value_type& val);
 		//用于配置空间并将数据进行填充
-		void allocate_and_fill(size_type n, const value_type& val);
+		iterator allocate_and_fill(size_type n, const value_type& val);
 		//后续要使用到构造和插入的辅助函数insert_aux,vector_aux
 		/*****************
 		此处待填写
@@ -130,7 +182,15 @@ namespace smart_stl
 		//private类型的相关函数
 		void deallocate();
 
+		//区分调用vector(InputIterator first, InputIterator last)的是指针还是整数，然后进行不同的处理
+		template<class InputIterator>
+		void vector_aux(InputIterator first, InputIterator last, _false_type);
 
+		template<class Integer>
+		void vector_aux(Integer n, const value_type& val, _true_type);
+
+		template<class InputIterator>
+		void allocate_and_copy(InputIterator first, InputIterator last);
 
 
 
@@ -138,7 +198,9 @@ namespace smart_stl
 	/**********************************vector相关实现***************************************************/
 	//由于模板类的函数声明和实现的其中一种方式是将声明与实现都放在.h文件中，一下便是vector的具体实现
 
-
+	
+	
+	/**************************涉及到构造函数、复制构造函数、析构函数**********************************/
 	template<class T, class Alloc>
 	vector<T, Alloc>::vector(const size_type n)
 	{
@@ -155,23 +217,35 @@ namespace smart_stl
 	template<class InputIterator>
 	vector<T, Alloc>::vector(InputIterator first, InputIterator last)
 	{
+		typedef typename is_Integer<InputIterator>:: class_type is_integer;
+		vector_aux(first, last, is_integer());
+	}
 
+	/*****
+	功能：vector复制构造函数
+	参数：被复制的vector对象v
+	返回值：无
+	******/
+	template<class T, class Alloc>
+	vector<T, Alloc>::vector(const vector& v)
+	{
+		allocate_and_copy(v.start_, v.finish_);
 	}
 
 
 	template<class T, class Alloc>
 	vector<T,Alloc>::~vector()
 	{
-		std::cout << "调用析构函数" << std::endl;
+		//我们在析构一个对象的时候，大致的思路都是这样的，先进行销毁、然后将该快内存块返还给内存池
+		smart_stl::destroy(start_, finish_);
+		deallocate();
 	}
 
 
+	/****************************************************************************************************/
 
-	/*需要说明一点，关于模板类函数在类外定义的时候需要带有特定的头部，如：
-	template<class T, class Alloc>
-	vector<T, Alloc>::
-	但是在函数的参数中，确实可以直接用value_type而不是T，是因为我们已经在类的声明中define了value_type是什么意思，所以这里可以直接用
-	*/
+
+
 	template<class T, class Alloc>
 	void vector<T, Alloc>::fill_initialize(size_type n, const value_type& val)
 	{
@@ -181,11 +255,12 @@ namespace smart_stl
 	}
 
 	template<class T, class Alloc>
-	iterator vector<T, Alloc>::allocate_and_fill(size_type n, const value_type& val)
+	typename vector<T, Alloc>::iterator vector<T, Alloc>::allocate_and_fill(size_type n, const value_type& val)
 	{
 		iterator result = data_allocator::allocate(n);
 		uninitialized_fill_n(result, n, val);
-		result = result - n;
+		//result = result - n;
+		return result;
 	}
 
 	template<class T, class Alloc>
@@ -197,6 +272,34 @@ namespace smart_stl
 			data_allocator::deallocate(start_, end_of_storage_ - start_);//end_of_storage_和end_符合前开后闭的特性
 		}
 	}
+
+
+	template<class T, class Alloc>
+	template<class InputIterator>
+	void vector<T, Alloc>::vector_aux(InputIterator first, InputIterator last, _false_type)
+	{
+		allocate_and_copy(first, last);
+	}
+
+
+	template<class T, class Alloc>
+	template<class Integer>
+	void vector<T, Alloc>::vector_aux(Integer n, const value_type& val, _true_type)
+	{
+		fill_initialize(n, val);
+	}
+
+
+	template<class T, class Alloc>
+	template<class InputIterator>
+	void vector<T, Alloc>::allocate_and_copy(InputIterator first, InputIterator last)
+	{
+		start_ = data_allocator::allocate(last - first);
+		finish_ = uninitialized_copy(first, last, start_);
+		end_of_storage_ = finish_;
+	}
+
+
 }
 
 #endif	_SMART_VECTOR_H_
