2ea7d2e1541cdb3ed99d3f725b4502ce670fe555
markjenny <lijianlin1990@sina.com>
2015-08-14 18:48:14 +0800

    finish the framework of rb_tree

--- Smart_STL/smart_RB_tree.h
+++ Smart_STL/smart_RB_tree.h
@@ -1,7 +1,9 @@
 #ifndef _SMART_RB_TREE_H_
 #define _SMART_RB_TREE_H_
-#include "smart_iterator_base.h"
 #include <stddef.h>
+#include <utility>
+#include "smart_iterator_base.h"
+#include "smart_alloc.h"
 //本节为构造底层数据红黑树，以学习为主，因为不太清除红黑树的删除过程（《算法》中关于节点删除的思想是理解的，但是具体还不太清楚，以此练手）
 namespace smart_stl
 {
@@ -183,6 +185,209 @@ namespace smart_stl
 	{
 		return lhs.base_ptr != rhs.base_ptr;
 	}
+
+	//构建红黑的数据结构,KeyofValue应该是一个仿函数
+	template<class Key, class Value, class KeyofValue, class Compare, class Alloc = alloc>
+	class rb_tree
+	{
+	public:
+		//首先是只有rb_tree需要用到的一些type
+		typedef void* void_pointer;
+		typedef rb_tree_node_base* base_ptr;
+		typedef rb_tree_node<Value> rbtree_node;
+		typedef simple_alloc<rbtree_node, Alloc> rb_tree_node_allocator;
+		typedef rb_tree_color_type color_type;
+		typedef rbtree_node* link_type;
+		typedef Key key_type;
+
+		//接着定义每个数据都需要的type：value_type，pointer等
+		typedef Value value_type;
+		typedef Value& reference;
+		typedef Value* pointer;
+		typedef const Value* const_pointer;
+		typedef const Value& const_reference;
+		typedef size_t size_type;
+		typedef ptrdiff_t distance_type;
+
+	private:
+		//构造节点等等，这点带有链表的数据结构都是一个德行
+		link_type get_node() //这个名字带波浪线是几个意思？我在这个类里面定义之后应该会覆盖的
+		{
+			return rb_tree_node_allocator::allocate();
+		}
+
+		void put_node(link_type p)
+		{
+			rb_tree_node_allocator::deallocate(p);
+		}
+
+		//创建节点
+		link_type create_node(const value_type& x)
+		{
+			link_type temp = get_node();
+			construct(&(temp->node_value), x);
+			return temp;
+		}
+
+		//复制节点：主要是复制节点的颜色和值
+		link_type clone_node(link_type x)
+		{
+			link_type temp = create_node(x->node_value);
+			temp->color = x->color;
+			temp->left = 0; //(在C++中，任何类型都是有0值得，另外BJ不是很喜欢宏~)
+			temp->right = 0;
+			return temp;
+		}
+
+		//删除节点
+		void destroy_node(link_type x)
+		{
+			destroy(&x->node_value);
+			put_node(x);
+		}
+
+	private:
+		//用三笔数据来表示整颗红黑树
+		size_type amountOfNode;
+		link_type header;
+		Compare key_compare;		//节点的比较规则（一般应该都是BST）
+
+	private:
+		//这里又利用了多态性，并且root、left等都是获得本体，那么就是引用，但是为什么《源码剖析中要用（link_type&）来转换呢？》
+		//与hearder相关
+		link_type& root() 
+		{
+			return (link_type)(header->parent);		//因为我们在其中的parent是base类型，需要转换一下指针的解释方式
+		}
+		link_type& leftmost()
+		{
+			return (link_type)(header->left);
+		}
+		link_type& rightmost()
+		{
+			return (link_type)(header->right);
+		}
+
+		//获取一个节点的相关信息
+		link_type& left(link_type& x)
+		{
+			return (link_type)(x->left);
+		}
+		link_type& right(link_type& x)
+		{
+			return (link_type)(x->right);
+		}
+		link_type& parent(link_type& x)
+		{
+			return (link_type)(x->parent);
+		}
+		reference value(link_type& x)
+		{
+			return x->node_value;
+		}
+		//因为这个key不可能改变，所以在函数的起始处加入const
+		const Key& key(link_type& x)
+		{
+			return KeyofValue()(x->node_value);
+		}
+		color_type& color(link_type& x)
+		{
+			return (color_type)(x->color);
+		}
+
+		//之前获取一个节点的信息是利用的派生类的指针，根据多态性可以考虑基类的指针
+		link_type& left(base_ptr& x)
+		{
+			return (link_type)(x->left);
+		}
+		link_type& right(base_ptr& x)
+		{
+			return (link_type)(x->right);
+		}
+		link_type& parent(base_ptr& x)
+		{
+			return (link_type)(x->parent);
+		}
+		color_type& color(base_ptr& x)
+		{
+			return (color_type)(x->color);
+		}
+		value_type& value(base_ptr& x)
+		{
+			return ((link_type)x)->node_value;
+		}
+		const Key& key(base_ptr& x)
+		{
+			return KeyofValue()(value(x));
+		}
+		link_type minimum(link_type x)		//个人觉得既然是iterator的完整版就不需要建立什么和base_ptr的关系
+		{
+			//在effective C++中的说法：link_type is a base_ptr
+			return (link_type) rb_tree_node_base::minimum(x);		//因为可能出现覆盖
+		}
+		link_type maximum(link_type x)
+		{
+			return (link_type) rb_tree_node_base::maximum(x);
+		}
+
+		//以上的函数都是可以通过之前所定义的那些类型来搞定，现在需要定义迭代器
+		public:
+			typedef rb_tree_iterator<value_type, reference, pointer> iterator;
+		private:
+			iterator _insert(base_ptr position, base_ptr x, const value_type& x);
+			//没搞懂copy得意义
+			link_type _copy(link_type x, link_type p);
+			void _erase(link_type x);		//因为要考虑到删除节点，那么久会调用这个点的node_value的析构函数，这样使用liink_type更加必要
+			void _init()
+			{
+				header = get_node();
+				//构造header上所需要的各种信息，因为不会用到header的值，所以这里不用初始化；
+				color(header) = rb_tree_red;
+				root() = 0;
+				rightmost() = header;
+				leftmost() = header;
+			}
+		public:
+			//现在需要构建普遍的容器所需要的六个方面的函数
+			//1.与构造，析构等有关；
+			rb_tree(const Compare& comp = Compare()) : amountOfNode(0), key_compare(comp) {_init();}
+			~rb_tree()
+			{
+				clear();
+				destroy_node(header);
+			}
+			rb_tree& operator= (const rb_tree& x);
+			//2.与逻辑比较有关；
+			//3.与容器的size有关；
+			bool empty() const 
+			{
+				return amountOfNode == 0;
+			}
+			size_type size() 
+			{
+				return amountOfNode;
+			}
+			size_type max_size()
+			{
+				return size_type(-1);
+			}
+			//下面这是什么鬼？
+			Compare key_comp() const {return key_compare;}
+			//4.与容器的迭代器有关；
+			iterator begin()
+			{
+				return leftmost();
+			}
+			iterator end()
+			{
+				return header;
+			}
+			//5.与容器的元素有关；但是对于树应该是没有的
+			//6.与改变容器相关；
+			pair<iterator, bool> insert_unique(const value_type& x);		//保持
+			iterator insert_equal(const value_type& x);
+	};
+
 
 }
 
